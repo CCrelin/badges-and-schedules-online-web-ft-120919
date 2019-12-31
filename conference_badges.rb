d18da2604ef4a5bb593839d27da4cdb66a153b50
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-12-31 06:33:55 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  new_array = []
+  attendees.each do |i| new_array << "Hello, my name is #{i}."
+  end
+  new_array
+end
+
+def assign_rooms(attendees)
+  new_array =[]
+  attendees.each_with_index do |i, index| new_array << "Hello, #{i}! You'll be assigned to room #{index+1}!"
+  end
+  new_array
+end 
+
+def printer(attendees)
+  badges_and_room_assignments.each_line do |line|
+  puts line.chomp
+  end
+  
+end
