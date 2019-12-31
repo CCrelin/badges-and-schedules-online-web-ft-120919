651c28d718da6c589511a65015c1bbca9d2cddbc
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-12-31 05:19:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,9 @@ end
 
 def batch_badge_creator(attendees)
   counter = 1 
+  array = attendees.join(", ")
   while counter < attendees.length do
-    return "Hello, my name is #{attendees}."
+    return "Hello, my name is #{array}."
     counter += 1
   end
 end
